7001409bb246f218667ddc4e3eb23a2fbc51a772
Ramon Kidd <kidd.ramon@gmail.com>
2024-08-13 21:57:14 -0400

    final commit before demo day

--- lhl-demo-streamlit.py
+++ lhl-demo-streamlit.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from vertexai.preview import batch_prediction
 
 # Load datasets
 train_data = pd.read_csv('data/train/sales_forecasting_train.csv')
 test_data = pd.read_csv('data/test/sales_forecasting_test.csv')
 sales_data = pd.read_csv('data/raw/synthetic_sales_data.csv')
+
 
 numerical_features = sales_data.select_dtypes(include=['int64', 'float64']).columns
 
@@ -15,28 +17,23 @@ rel_plot_img = 'images/rel_plot.png'
 prediction_vs_ground_truth_img = 'images/prediction_vs_ground_truth.png'
 eval_results_img = 'images/model_eval_results_gcp.png'
 architecture = 'images/final_project_architecture.png'
+storefront = 'images/storefront.jpeg'
+batch_prediction = 'images/batch_prediction_screenshot.png'
+vertex_code = 'images/vertex_code_screenshot.png'
 
 # Set Streamlit app title
 st.title("🌟 AutoML Hierarchical Forecasting with Vertex AI")
 
 # Sidebar for navigation
 st.sidebar.title("🚀 The Journey")
-page = st.sidebar.radio("Go to", ["Introduction", "The Data", "Model Training in Vertex AI", "Model Results", "Batch Predictions"])
+page = st.sidebar.radio("Go to", ["Introduction", "The Architecture", "The Data", "Model Training in Vertex AI", "Model Results", "Batch Predictions"])
 
 # Problem Statement
 if page == "Introduction":
     st.header("📖 ❓ Intro & Problem Statement")
-    st.write("""
-    In today's dynamic retail environment, accurately forecasting sales is crucial for optimizing inventory levels, 
-    minimizing stockouts, and maximizing revenue. This project addresses the challenge of predicting future sales 
-    across multiple products and store locations for a fictitious organization, accounting for various factors such as seasonality, product type, 
-    and store location.
+    st.image(storefront, use_column_width=True)
 
-    The goal of this project is to build a robust machine learning model using Google Cloud's Vertex AI AutoML, 
-    leveraging historical sales data to forecast future sales. The model will assist retailers in making data-driven 
-    decisions to improve operational efficiency and profitability.
-    """)
-
+if page == "The Architecture":
     st.header("Architecture")
     st.image(architecture, use_column_width=True)
 
@@ -104,23 +101,10 @@ if page == "The Data":
 # Model Cretion in Vertex AI
 if page == "Model Training in Vertex AI":
     st.header("🛠️ Model Training in Vertex AI")
+    st.image(vertex_code, use_column_width=True)
 
 
-    st.write("""
-        The Vertex AI AutoML Forecasting job was configured to build a sales prediction model by leveraging historical sales data.
 
-        ### Key Configuration Details:
-
-        - **Optimization Objective**: The model was trained to minimize Root Mean Squared Error (RMSE), focusing on reducing large prediction errors.
-
-        - **Time Series Setup**: The model used a 30-day context window to predict the next 30 days, capturing short-term trends and seasonal patterns.
-
-        - **Feature Importance**: Key features such as `store`, `product_type`, and `date` were utilized to enhance the model's accuracy, with `store` being the most influential predictor of sales.
-
-        - **Hierarchical Grouping**: The model also leveraged hierarchical grouping by product to make more robust predictions across different product categories.
-
-        This setup allowed the model to effectively capture the temporal dynamics and key factors influencing sales, resulting in accurate and actionable forecasts.
-        """)
 
 # Model Results - Model Evaluation Section
 if page == "Model Results":
@@ -129,61 +113,15 @@ if page == "Model Results":
     st.write("### 🔍 VertexAI model using AutoML in Google Cloud")
     st.image(eval_results_img, caption='Prediction vs Ground Truth', use_column_width=True)
 
-    # st.write("### 📈 Model Evaluation")
-    # st.write("""
-    # The Vertex AI model used regression to predict sales. The model captured the trend and seasonality
-    # effectively but showed some discrepancies in magnitude for certain products and store locations.
-    # """)
-    #
-    # # Key Performance Metrics
-    # st.write("### Key Performance Metrics")
-    # st.write("""
-    # - **Mean Absolute Error (MAE):** 1.327
-    #   On average, the model's predictions are off by 1.327 units of sales.
-    #
-    # - **Mean Absolute Percentage Error (MAPE):** 5.732
-    #   The model's predictions are off by approximately 5.732% relative to the actual sales values.
-    #
-    # - **Root Mean Squared Error (RMSE):** 1.93
-    #   The RMSE indicates that the model's predictions have an average squared difference from the actual values of 1.93 units.
-    #
-    # - **Root Mean Squared Logarithmic Error (RMSLE):** 0.074
-    #   RMSLE penalizes under-predictions more than over-predictions, making it useful when the target variable can have large variations or outliers.
-    #
-    # - **R-Squared (R²):** 0.992
-    #   The model explains 99.2% of the variance in the sales data, indicating very high explanatory power.
-    # """)
-    #
-    # # Feature Importance
-    # st.write("### Feature Importance")
-    # st.write("""
-    # The model identified the following features as the most important for predicting sales:
-    #
-    # - **Store:** The most significant feature, contributing over 50% to the model's decision-making process. This indicates that store location or type is crucial for sales predictions.
-    #
-    # - **Product Type:** Contributes around 20%, highlighting the importance of the type of product (e.g., seasonal products) in sales forecasting.
-    #
-    # - **Date:** Contributes about 10%, reflecting the importance of time-related patterns like seasonality.
-    #
-    # - **Product and Product Category:** These features are less important but still contribute to the model's predictions, indicating some influence of specific product characteristics on sales.
-    # """)
-    #
-    # # Interpretation and Recommendations
-    # st.write("### Interpretation and Recommendations")
-    # st.write("""
-    # - **High Model Accuracy:** The model's low MAE, MAPE, and RMSE, coupled with a high R², indicate strong predictive performance and accuracy.
-    #
-    # - **Focus on Store-Level Strategies:** Given the high importance of the `store` feature, retailers should prioritize store-level strategies, such as optimizing inventory and tailoring marketing efforts.
-    #
-    # - **Product and Seasonal Strategy:** The significance of `product_type` and `date` suggests that different products have varying sales patterns over time, which should inform inventory and marketing plans.
-    #
-    # - **Further Model Improvement:** Although the model performs well, further fine-tuning could enhance prediction accuracy, particularly for specific products or underrepresented stores.
-    # """)
+
 
 
 # Batch Predictions
 if page == "Batch Predictions":
-    st.header("📦 Batch Predictions")
+    st.header("📦 VertexAI batch job")
+    st.image(batch_prediction, use_column_width=True)
+
+    st.header("📦 Job Results")
     st.image(prediction_vs_ground_truth_img, caption='Batch Predictions Process in Vertex AI', use_column_width=True)
     st.write("""
         The comparison between predicted and actual sales shows that the model generally captures the overall seasonal and temporal trends effectively.
